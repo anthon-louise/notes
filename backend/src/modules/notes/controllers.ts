c144791b68c34e1d270384c793e2b3d9c6a599fc
anthon-dev <anthonlouisenoynay123@example.com>
2026-07-23 07:49:55 +0800

    add get all notes api

--- backend/src/modules/notes/controllers.ts
+++ backend/src/modules/notes/controllers.ts
@@ -2,9 +2,11 @@ import asyncHandler from "express-async-handler";
 import { Request, Response, NextFunction } from "express";
 import { createNoteSchema } from "./schemas.js";
 import { pool } from "../../config/db.js";
+import { Note } from "./types.js";
 
 export const createNote = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  const {title, content} = createNoteSchema.parse(req.body);
+
+  const { title, content } = createNoteSchema.parse(req.body);
 
   await pool.query(`
     INSERT INTO
@@ -15,4 +17,20 @@ export const createNote = asyncHandler(async (req: Request, res: Response, next:
   res.status(201).json({
     message: "Note Created"
   })
+
+})
+
+
+export const getNotes = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+
+  const noteResults = await pool.query<Note>(`
+    SELECT title, content, created_at
+    FROM notes
+    `);
+
+  res.status(200).json({
+    message: "Notes fetched",
+    notes: noteResults.rows
+  });
+
 })
